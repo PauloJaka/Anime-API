703152a76479876083aeafb8211da778e0c1af83
PauloJaka <zpaulogamer@gmail.com>
2023-07-14 16:08:56 -0300

    Script alteração e nova modularização do request da API

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Lista de valores para alternar no URL
-const animeIds = [38000, 40748, 39535];
+const animeIds = [38000, 40748, 39535, 5114, 41467, 11061, 37521, 21, 54898, 44511, 30276, 31181, 20583, 54112, 37105, 49596, 12115, 34572, 22199, 199, 431, 164, 2890, 32281];
+
 const animeDataArray = [];
 
 // Função para realizar o fetch com um determinado ID de anime
@@ -9,44 +10,23 @@ function fetchAnimeData(animeId) {
   fetch(url)
     .then(response => response.json())
     .then(animeData => {
-      const genres = animeData.data.genres;
+      const genres = animeData.data.genres.map(genre => genre.name);
       const titles = animeData.data.titles[0];
       const imageUrl = animeData.data.images.webp.image_url;
       const popularity = animeData.data.popularity;
       const trailer = animeData.data.trailer.url;
       const synopsis = animeData.data.synopsis;
 
-      
-      const allGenres = genres.map(genre => genre.name)
-
       const animeObject = {
         title: titles.title,
-        genres: allGenres,
+        genres: genres,
         imageUrl: imageUrl,
         popularity: popularity,
         trailer: trailer,
         synopsis: synopsis
       };
 
-      // Adicionando o objeto ao array
       animeDataArray.push(animeObject);
-
-      // Criando elementos HTML para exibir as informações
-      const animeElement = document.createElement('div');
-      const titleElement = document.createElement('h2');
-      const imageElement = document.createElement('img');
-
-      // Definindo as informações do anime
-      titleElement.innerText = titles.title;
-      titleElement.style.fontWeight = "bold"; // Adicionando estilo CSS
-      titleElement.style.fontSize = "24px"; // Definindo tamanho da fonte
-
-      imageElement.src = imageUrl;
-
-      // Adicionando os elementos ao DOM
-      animeElement.appendChild(titleElement);
-      animeElement.appendChild(imageElement);
-      document.body.appendChild(animeElement);
     })
     .catch(error => console.log(error));
 }

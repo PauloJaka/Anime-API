bbc34fea8f8caf69fe2e73d3280cbedd2a25a98b
PauloJaka <zpaulogamer@gmail.com>
2023-07-16 15:49:47 -0300

    SCRIPT adicionado seção anime, unto  com alterações na função do swapper para ser responsiva.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 const animeIds = [38000, 40748, 39535, 5114, 41467, 11061, 37521, 21, 54898, 44511, 30276, 31181, 20583, 54112, 37105, 49596, 12115, 34572, 22199, 199, 431, 164, 2890, 32281];
+
 const animeDataArray = [];
 
-// Função para realizar o fetch com um determinado ID de anime
 async function fetchAnimeData(animeId) {
   const url = `https://api.jikan.moe/v4/anime/${animeId}/full`;
 
@@ -11,11 +11,14 @@ async function fetchAnimeData(animeId) {
 
     const genres = animeData.data.genres.map(genre => genre.name);
     const titles = animeData.data.titles[0];
-    const imageUrl = animeData.data.images.webp.image_url;
+    const imageUrl = animeData.data.images.webp.small_image_url;
     const popularity = animeData.data.popularity;
     const trailer = animeData.data.trailer.url;
     const synopsis = animeData.data.synopsis;
     const largeImageUrl = animeData.data.images.webp.large_image_url;
+    
+    const firstSentence = synopsis.split('.')[0].slice(0, -1);
+
 
     const animeObject = {
       title: titles.title,
@@ -23,7 +26,7 @@ async function fetchAnimeData(animeId) {
       imageUrl: imageUrl,
       popularity: popularity,
       trailer: trailer,
-      synopsis: synopsis,
+      synopsis: firstSentence,
       sectionImages : largeImageUrl
     };
 
@@ -71,8 +74,10 @@ const createBoxDiv = (imageUrl) => {
   return div;
 };
 
-const createContentDiv = () => {
-  const div = createDiv('content');
+const createBoxDivAnimeSection = (imageUrl) => {
+  const div = createDiv('box');
+  div.style.background = `url(${imageUrl})`;
+  div.style.backgroundRepeat = 'no-repeat';
   return div;
 };
 
@@ -82,9 +87,9 @@ const createH3element = (title) => {
   return h3;
 };
 
-const createP = (Synopsis) => {
+const createP = (synopsis) => {
   const p = document.createElement('p');
-  p.textContent = Synopsis
+  p.textContent = synopsis
   return p;
 };
 
@@ -96,32 +101,116 @@ const createA = () => {
 };
 
 let sectionAnimes = 0;
+let slidesSectionAnimes = 5;
 
 const sectionSlides = () => {
   const wrapper = document.querySelector('.swiper-wrapper');
   const slideDiv = createDiv('swiper-slide');
   const boxDiv = createBoxDiv(animeDataArray[sectionAnimes].sectionImages);
-  const contentDiv = createContentDiv();
+  const contentDiv = createDiv('content');
   const h3Element = createH3element(animeDataArray[sectionAnimes].title);
   const pElement = createP(animeDataArray[sectionAnimes].synopsis);
   const aElement = createA();
-
+  
+  wrapper.appendChild(slideDiv);
+  slideDiv.appendChild(boxDiv);
+  boxDiv.appendChild(contentDiv);
   contentDiv.appendChild(h3Element);
   contentDiv.appendChild(pElement);
   contentDiv.appendChild(aElement);
-  boxDiv.appendChild(contentDiv);
-  slideDiv.appendChild(boxDiv);
-  wrapper.appendChild(slideDiv);
+
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
-  await fetchAwaitAnimeData(); // Espera até que os dados dos animes sejam carregados
-  for (let i = 0; i < 4; i++) {
+  await fetchAwaitAnimeData(); 
+  for (let i = 0; i < 5; i++) {
     sectionSlides();
     sectionAnimes++;
   }
 });
 
+const animesSectionSlides = () => {
+  const swiperWrapper = document.querySelector('.swiper.anime-slider .swiper-wrapper');
+  const slideDiv = createDiv('swiper-slide');
+  const boxDiv = createBoxDivAnimeSection(animeDataArray[slidesSectionAnimes].sectionImages);
+  const contentDiv = createDiv('content');
+  const h3Element = createH3element(animeDataArray[slidesSectionAnimes].title);
+  const pElement = createP(animeDataArray[slidesSectionAnimes].synopsis);
+  const aElement = createA();
+
+  swiperWrapper.appendChild(slideDiv);
+  slideDiv.appendChild(boxDiv);
+  boxDiv.appendChild(contentDiv);
+  contentDiv.appendChild(h3Element);
+  contentDiv.appendChild(pElement);
+  contentDiv.appendChild(aElement);
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchAwaitAnimeData()
+    .then(() => {
+      for (let i = 0; i < 10; i++) {
+        animesSectionSlides();
+        slidesSectionAnimes++;
+      }
+    })
+    .catch(error => console.log(error));
+});
+
+
+
+var swiper = new Swiper(".home-slider", {
+  loop:false,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    stopOnLastSlide: false,
+  },
+});
+
+const updateSwiper = () => {
+  const windowWidth = window.innerWidth;
+
+  let slidesPerView = getSlidesPerView(windowWidth);
+
+  swiper.params.slidesPerView = slidesPerView;
+  swiper.update();
+};
+
+const getSlidesPerView = (windowWidth) => {
+  if (windowWidth >= 992) {
+    return 3;
+  } else if (windowWidth >= 768) {
+    return 2;
+  } else {
+    return 1;
+  }
+};
+
+var swiper = new Swiper(".anime-slider", {
+  slidesPerView: getSlidesPerView(window.innerWidth),
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: false
+});
+
+window.addEventListener('resize', updateSwiper);
+updateSwiper();
 
 
 
